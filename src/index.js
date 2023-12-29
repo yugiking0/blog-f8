b82de4057407c197a3a2919229687ecbd4241ebe
unknown <yugiking0@gmail.com>
2023-12-29 16:15:17 +0700

    Clean Clear Code remove commands

--- src/index.js
+++ src/index.js
@@ -9,22 +9,14 @@ const handlebars = require('express-handlebars');
 app.use(morgan('combined'));
 
 // Template Engine Handlebars
-// console.log('PATCH:::',path.join(__dirname, '/resources/views'));
 app.engine('.hbs', handlebars.engine({ 
     extname: '.hbs' 
   }));
 app.set('view engine', '.hbs');
-
-// app.engine('handlebars', handlebars.engine());
-// app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '/resources/views'));
-// app.set('views', './views');
+
 
 // Express Server
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
 app.get('/', (req, res) => {
    res.render(`home`);
 });
