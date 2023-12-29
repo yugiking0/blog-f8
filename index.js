fe2d876dc2fc1a92f358852008ad07d9b7385f3c
unknown <yugiking0@gmail.com>
2023-12-29 10:51:46 +0700

    Update Morgan Logger Middleware

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
 
-app.get('/trang-chu', (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c =  a + b;
+// Middle Ware Logger
+app.use(morgan('combined'));
 
+// Express Server
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
